a130aed88b0e965fc5cd6bfcaa512e582c9524d2
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-02-14 20:52:15 -0800

    Create portalgenerator.py

---
+++ scripts/portalgenerator.py
@@ -0,0 +1,18 @@
+import random
+
+#random.seed(1)
+
+areas = ["A","B","C","D","E","F","G","H"]
+random.shuffle(areas)
+
+portals = []
+
+start = areas.pop() # Start with a random area
+
+while len(areas) > 0: # Connect to a different random area
+    end = areas.pop()
+    portals.append((start,end))
+    start = end 
+  
+# Connect first and last area as a special case
+portals.append((portals[0][0], portals[-1][-1]))
