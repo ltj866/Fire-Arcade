0321e18523f8065010d2422c0da8ebf002f1122d
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-02-18 02:38:09 -0800

    food places without coordinates

--- games/Phaser-Snake-Game/public/js/snake.js
+++ games/Phaser-Snake-Game/public/js/snake.js
@@ -60,12 +60,13 @@ function create ()
 
         initialize:
 
-        function Food (scene, x, y)
+        function Food (scene)
         {
+
             Phaser.GameObjects.Image.call(this, scene)
 
             this.setTexture('blocks', 1);
-            this.setPosition(x * GRID, y * GRID);
+            this.move();
             this.setOrigin(0);
 
             this.points = 100;
@@ -117,13 +118,12 @@ function create ()
         {
         
             this.alive = true;
-            //this.parts = scene.add.group();
             this.body = []
             this.head = scene.add.image(x * GRID, y * GRID, 'blocks', 0);
             this.head.setOrigin(0);
             this.body.push(this.head);
 
-            // Trying to get one body part to follow correctly. WORKING HERE
+            // Trying to get one body part to follow correctly.
             //var part = scene.add.image((x-1) * 32, y * 32, 'blocks', 1);
             //part.setOrigin(0);
             //this.body.push(part);
@@ -137,7 +137,7 @@ function create ()
         update: function (time)
         {
             
-            //if (time >= this.moveTime)
+            //if (time >= this.moveTime) Why is this here, does it do anything?
             //{
             //    return this.move(time);
             //}
@@ -171,16 +171,22 @@ function create ()
     });
 
     snake = new Snake(this, 8, 8);
+    
     // x = width 25 grid
     // y width 19
 
+    for (let i = 0; i <= 25; i++) {
+        wall = new Wall(this, i, 0);
+        wall = new Wall(this, i, 19);
+      }
+    
     var wall = new Wall(this, 10, 10);
     var wall = new Wall(this, 10, 11);
     var wall = new Wall(this, 10, 12);
     var wall = new Wall(this, 10, 13);
     var wall = new Wall(this, 10, 14);
 
-    var food0 = new Food(this, 25, 19);
+    var food0 = new Food(this);
     var food1 = new Food(this, 14, 10);
     var food2 = new Food(this, 5, 13);
 
