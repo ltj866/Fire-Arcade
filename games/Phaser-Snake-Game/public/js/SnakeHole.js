aff566daa29a449192c573dd79b87282486dc2e0
Holdenian <holdenaatlo@gmail.com>
2024-03-27 14:18:59 -0700

    Animated Wrap Blocks
    -added new frames/animation to wrap blocks (are now 12 frames each instead of 3)

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -332,50 +332,50 @@ class GameScene extends Phaser.Scene {
         //WRAP_BLOCK_ANIMS
         this.anims.create({
             key: 'wrapBlock01',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 0, 1, 2, 1]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9, 10 ,11]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock02',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 3, 4, 5, 4]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 12,13,14,15,16,17,18,19,20,21,22,23]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock03',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 6, 7, 8, 7]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 24,25,26,27,28,29,30,31,32,33,34,35]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock04',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 9, 10, 11, 9]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 36,37,38,39,40,41,42,43,44,45,46,47]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock05',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 12, 13, 14, 13]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 48,49,50,51,52,53,54,55,56,57,58,59]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock06',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 15, 16, 17, 16]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 60,61,62,63,64,65,66,67,68,69,70,71]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock07',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 18, 19, 20, 19]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 72,73,74,75,76,77,78,79,80,81,82,83]}),
+            frameRate: 8,
             repeat: -1
         })
         this.anims.create({
             key: 'wrapBlock08',
-            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 21, 22, 23, 22]}),
-            frameRate: 4,
+            frames: this.anims.generateFrameNumbers('wrapBlockAnim',{ frames: [ 84,85,86,87,88,89,90,91,92,93,94,95]}),
+            frameRate: 8,
             repeat: -1
         })
         // #endregion
