8c367ddf4df1eeb249cf2a019237d5a7431a1e88
Holdenian <holdenaatlo@gmail.com>
2024-04-25 15:46:40 -0700

    Tiled Cleanup
    - Omitted unneeded layers
    - deleted unneeded tilemaps
    - fixed stage 9 portals

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -14,7 +14,7 @@ import {PORTAL_COLORS} from './const.js';
 const GAME_VERSION = 'v0.5.04.19.006';
 export const GRID = 24;        //.................... Size of Sprites and GRID
 var FRUIT = 5;                 //.................... Number of fruit to spawn
-export const LENGTH_GOAL = 28; //28..................... Win Condition
+export const LENGTH_GOAL = 2; //28..................... Win Condition
 const  STARTING_LIVES = 12;
 
 
