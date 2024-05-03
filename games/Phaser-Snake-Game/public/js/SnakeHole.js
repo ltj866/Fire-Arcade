32f5bbb08ec22a8e03af9a6d4e57b51068476ce3
Holdenian <holdenaatlo@gmail.com>
2024-05-03 09:48:50 -0700

    Fix

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -14,7 +14,7 @@ import {PORTAL_COLORS} from './const.js';
 const GAME_VERSION = 'v0.5.05.03.001';
 export const GRID = 24;        //.................... Size of Sprites and GRID
 //var FRUIT = 5;                 //.................... Number of fruit to spawn
-export const LENGTH_GOAL = 28; //28..................... Win Condition
+export const LENGTH_GOAL = 5; //28..................... Win Condition
 const  STARTING_ATTEMPTS = 25;
 
 
