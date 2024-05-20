c7e53cf439433a851b89e3e9b643fe017d668020
Holdenian <holdenaatlo@gmail.com>
2024-05-20 15:20:48 -0700

    Switch Case/Portal to Tune Light

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -652,7 +652,7 @@ class GameScene extends Phaser.Scene {
         //var boostTrailX = this.add.sprite(24, 72).play("boostTrailX01").setOrigin(0,0)
         
         this.lights.enable();
-        this.lights.setAmbientColor(0xF6F6F6);
+        this.lights.setAmbientColor(0xC9C9C9);
         
 
         //wrapBlock03.play("wrapBlock03")
@@ -1103,7 +1103,37 @@ class GameScene extends Phaser.Scene {
 
         // #endregion
         this.portals.forEach(portal => {
-            this.lights.addLight(portal.x +16, portal.y + 16, 128,  portal.tintTopLeft).setIntensity(1);
+            var portalLightColor = 0xFFFFFF;
+            switch (portal.tintTopLeft) {
+                case 0xFF0000:
+                    portalLightColor = 0xFF0000;
+                    break;
+                case 0xff9900:
+                    portalLightColor = 0xC05D00;
+                    break;
+                case 0xffff00:
+                    portalLightColor = 0xC5C500;
+                    break;
+                case 0x00ff00:
+                    portalLightColor = 0x00ff00;
+                    break;
+                case 0x00ffff:
+                    portalLightColor = 0x00FFFF;
+                    break;   
+                case 0x4a86e8:
+                    portalLightColor = 0x074FEA;
+                    break;        
+                case 0x9900ff:
+                    portalLightColor = 0x9900FF;
+                    break;
+                case 0xff00ff:
+                    portalLightColor = 0xFF00FF;
+                    break; 
+                default:
+                    console.log("default portal color break")
+                    break;
+            }
+            this.lights.addLight(portal.x +16, portal.y + 16, 128,  portalLightColor).setIntensity(1);
             this.add.particles(portal.x, portal.y, "portalParticle01", {
                 color: [ portal.tintTopLeft,0x000000, 0x000000],
                 colorEase: 'quad.out',
