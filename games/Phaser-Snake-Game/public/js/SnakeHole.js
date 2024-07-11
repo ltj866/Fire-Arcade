61e9bffc4dac6af1d6fae009575f53af4c4e16e4
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-07-11 14:02:55 -0700

    bottom row testing.

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -248,7 +248,7 @@ export const GState = Object.freeze({
 const DREAMWALLSKIP = [0,1,2];
 
 // #region START STAGE
-const START_STAGE = 'World_1-1'; // Warning: Cap sensitive in the code but not in Tiled. Can lead to strang bugs.
+const START_STAGE = 'r-07-10-6'; // Warning: Cap sensitive in the code but not in Tiled. Can lead to strang bugs.
 var END_STAGE = 'Stage-06'; // Is var because it is set during debugging UI
 
 
