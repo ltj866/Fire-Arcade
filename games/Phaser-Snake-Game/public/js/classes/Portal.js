f05a226fdcef463018e3e7b3d1c382a81663425f
Holdenian <holdenaatlo@gmail.com>
2024-10-21 11:43:47 -0700

    Portal Highlight Depth Change

--- games/Phaser-Snake-Game/public/js/classes/Portal.js
+++ games/Phaser-Snake-Game/public/js/classes/Portal.js
@@ -26,8 +26,8 @@ var Portal = new Phaser.Class({
         if (anim === "portalForm") {
             this.setOrigin(.3125,.3125);
             scene.portals.push(this);
-            this.portalHighlight = scene.add.sprite(from[0], from[1]).setDepth(54).setOrigin(.3125,.3125);
-            this.portalHighlight.setTint(color.color);
+            this.portalHighlight = scene.add.sprite(from[0], from[1]).setDepth(46).setOrigin(.3125,.3125);
+            //this.portalHighlight.setTint(color.color);
             //this.portalHighlight.chain(['portalHighlights']);
             
 
