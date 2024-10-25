049b852aa26cd865afaa09304bfe3c5ba14c951f
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-10-24 17:58:00 -0700

    level update and reset to 1-1

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -3034,7 +3034,7 @@ class GameScene extends Phaser.Scene {
                 ourGameScene.extractPanel.setAlpha(0);
                 console.log("YES");
                 ourGameScene.extractMenuOn = false;
-                ourGameScene.finalScore()
+                ourGameScene.finalScore("MainMenuScene", {});
                 return true;
             },
             'NO': function () {  
@@ -3070,8 +3070,20 @@ class GameScene extends Phaser.Scene {
             },
             'LOOP TO ORIGIN': function () {
                 // TODO: send to origin
+                ourGameScene._menuElements.forEach(textElement =>{
+                    textElement.setAlpha(0);
+                });
+                ourGameScene.extractPromptText.setAlpha(0);
+                ourGameScene.extractPanel.setAlpha(0);
                 console.log("LOOP");
                 ourGameScene.extractMenuOn = false;
+
+                // Clear for reseting game   
+                ourGameScene.finalScore("GameScene", {
+                    stage: START_STAGE,
+                    score: 0,
+                    startupAnim: true,
+                });
                 return true;
             },
         }
@@ -5281,7 +5293,7 @@ class GameScene extends Phaser.Scene {
         this._selected = this._menuElements[this.exCursorIndex];
     }
 
-    finalScore(){
+    finalScore(nextScene, args){
         const ourStartScene = this.scene.get('StartScene');
 
         this.extractHole[0].play('extractHoleClose');
@@ -5371,7 +5383,9 @@ class GameScene extends Phaser.Scene {
               });
 
             const onContinue = function () {
-                ourGameScene.scene.start('MainMenuScene');
+                ourGameScene.scene.get("StartScene").stageHistory = [];
+                ourGameScene.scene.get("PersistScene").coins = START_COINS;
+                ourGameScene.scene.start(nextScene, args); 
             }
             this.input.keyboard.on('keydown-SPACE', function() { 
                 onContinue();
