51bfab6360869847148f7e3783d0091ddfe10f90
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-11-25 23:03:48 -0800

    Fixed stage redo bug.

--- games/Phaser-Snake-Game/public/js/data/quickMenus.js
+++ games/Phaser-Snake-Game/public/js/data/quickMenus.js
@@ -82,11 +82,11 @@ export var QUICK_MENUS = new Map([
                 //ourGameScene.events.off('spawnBlackholes');
                 //ourGameScene.scene.get("InputScene").scene.restart();
 
-                var previous = ourGameScene.scene.get("SpaceBoyScene").stageHistory.pop();
+                var previous = ourGameScene.scene.get("PersistScene").stageHistory.pop();
                 if (previous != undefined) {
                     if (ourGameScene.stage != previous.stage) {
                         // Put It back
-                        ourGameScene.scene.get("SpaceBoyScene").stageHistory.push(previous);
+                        ourGameScene.scene.get("PersistScene").stageHistory.push(previous);
                     } else {
                         // Leave it out so you can run it again.
                     }
